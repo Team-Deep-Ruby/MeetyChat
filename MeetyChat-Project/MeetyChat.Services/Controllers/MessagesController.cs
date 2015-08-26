8f9963419af368cc547bff0ce9704bd64fad1714
mihayloff <mihayloff14@gmail.com>
2015-08-26 14:41:33 +0300

    added initial messages controller.

---
+++ MeetyChat-Project/MeetyChat.Services/Controllers/MessagesController.cs
@@ -0,0 +1,35 @@
+﻿namespace MeetyChat.Services.Controllers
+{
+    using System;
+    using System.Web.Http;
+    using Data.Interfaces;
+    using Infrastructure;
+
+    public class MessagesController : BaseApiController
+    {
+        private IUserIdProvider userIdProvider;
+
+        public MessagesController(IMeetyChatData data, IUserIdProvider userIdProvider)
+            : base(data)
+        {
+            this.userIdProvider = userIdProvider;
+        }
+
+        [Route("api/rooms/{roomId}/messages")]
+        public IHttpActionResult GetAllMessages(string roomId)
+        {
+            Guid roomGuid;
+            try
+            {
+                roomGuid = Guid.Parse(roomId);
+            }
+            catch (FormatException)
+            {
+                return this.BadRequest("Invalid room id.");
+            }
+            
+            return this.Ok("asdasdas");
+
+        }
+    }
+}
